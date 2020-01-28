517bedb2660c7dd1aef6e1b88c105f107f7bef1a
pornnipaj <pornniparj@gmail.com>
2020-01-28 14:18:22 +0700

    Update webservice.service.ts

--- src/app/webservice.service.ts
+++ src/app/webservice.service.ts
@@ -7,17 +7,58 @@ import { HttpClient, HttpRequest, HttpEvent, HttpHeaders } from '@angular/common
 export class WebserviceService {
   
   apiLocal_url = 'http://localhost:6369';
+
   apiServer_url = 'https://cors-anywhere.herokuapp.com/http://wms.wingplusweb.com/';
 
   constructor(private http: HttpClient) { }
 
    //#region Local
-  login(user) {
+  // login(user) {
+  //   return new Promise((resovle, reject) => {
+
+  //     let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  //     this.http.post(this.apiLocal_url + '/API/Login.ashx' + '?email=' + user.email + '&password=' + user.password + '&type=' + user.type,
+  //       JSON.stringify(user), option).subscribe(data => {
+  //         resovle(data);
+  //       }, error => {
+  //         reject(error)
+  //       });
+  //   });
+  // }
+
+  // tranfer(form){
+  //   return new Promise((resovle, reject) => {
+  //         let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
+    
+  //         this.http.post(this.apiLocal_url + '/API/Tranfer.asmx/tranfers', JSON.stringify(form), option).subscribe(data => {
+  //           resovle(data);
+  //         }, error => {
+  //           reject(error)
+  //         });
+  //       });
+  // }
+
+  // incoming(form){
+  //   return new Promise((resovle, reject) => {
+  //         let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
+    
+  //         this.http.post(this.apiLocal_url + '/API/Incoming.asmx/incomingpage', JSON.stringify(form), option).subscribe(data => {
+  //           resovle(data);
+  //         }, error => {
+  //           reject(error)
+  //         });
+  //       });
+  // }
+  //#endregion
+
+   //#region Server
+   login(user) {
     return new Promise((resovle, reject) => {
 
       let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-      this.http.post(this.apiLocal_url + '/API/Login.ashx' + '?email=' + user.email + '&password=' + user.password + '&type=' + user.type,
+      this.http.post(this.apiServer_url + '/API/Login.ashx' + '?email=' + user.email + '&password=' + user.password + '&type=' + user.type,
         JSON.stringify(user), option).subscribe(data => {
           resovle(data);
         }, error => {
@@ -30,7 +71,7 @@ export class WebserviceService {
     return new Promise((resovle, reject) => {
           let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
     
-          this.http.post(this.apiLocal_url + '/API/Tranfer.asmx/tranfers', JSON.stringify(form), option).subscribe(data => {
+          this.http.post(this.apiServer_url + '/API/Tranfer.asmx/tranfers', JSON.stringify(form), option).subscribe(data => {
             resovle(data);
           }, error => {
             reject(error)
@@ -42,7 +83,7 @@ export class WebserviceService {
     return new Promise((resovle, reject) => {
           let option: any = new HttpHeaders({ 'Content-Type': 'application/json' });
     
-          this.http.post(this.apiLocal_url + '/API/Incoming.asmx/incomingpage', JSON.stringify(form), option).subscribe(data => {
+          this.http.post(this.apiServer_url + '/API/Incoming.asmx/incomingpage', JSON.stringify(form), option).subscribe(data => {
             resovle(data);
           }, error => {
             reject(error)
